393ede9921776ea195283eb01b73dcffa0aadb5f
tilak.sasmal999 <tilak.sasmal999@gmail.com>
2025-01-24 12:13:25 +0000

    added new cron

---
+++ cron-jobs/refresh-domain-status.ts
@@ -0,0 +1,20 @@
+import { Logger } from "pino"
+import { Job } from "../types/cron"
+import { validateEnvParams } from "../lib/utils"
+
+async function main(logger: Logger) {
+    const { API_KEY, API_ENDPOINT } = validateEnvParams()
+    const url = `${API_ENDPOINT}/api/management/checkDomainSetup`
+    const options = {
+        method: "GET",
+        headers: { "x-api-key": API_KEY },
+    }
+    const result = await fetch(url, options)
+    const data = await result.json()
+    logger.info(data, "[refreshDomainStatus] Cron executed with result")
+}
+
+export default {
+    schedule: process.env.BACKUP_CRON || "*/2 * * * *",
+    job: main,
+} as Job
